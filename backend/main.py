c7113d45cae371402db13c97b4b60ee8c1eaeeb9
Uzair <uzairnaeem@folio3.com>
2024-07-24 21:19:06 -0500

    Task 3

---
+++ backend/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
+
+class VideoAnalysisRequest(BaseModel):
+    youtube_link: HttpUrl
+
+app = FastAPI()
+
+@app.post("/analyze_video")
+def analyze_video(request: VideoAnalysisRequest):
+    from langchain_community.document_loaders import YoutubeLoader
+    from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+    loader = YoutubeLoader.from_youtube_url(str(request.youtube_link), add_video_info = True)
+    docs = loader.load()
+    print(f"on load: {type(docs)}")
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 0)
+    result = text_splitter.split_documents(docs)
+
+    print(f"{type(result)}")
+
+    author = result[0].metadata['author']
+    length = result[0].metadata['length']
+    title = result[0].metadata['title']
+    total_size = len(result)
+
+    return {
+        "author" : author,
+        "length" : length,
+        "title" : title,
+        "total_size" : total_size
+
+    }
